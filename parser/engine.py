778e123401b2d1475e513dbac6835825b71575f0
OviKosta <ovikosta@gmail.com>
2019-04-18 18:33:27 +0300

    Create engine parser.

--- parser/engine.py
+++ parser/engine.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 import re
+from collections import Counter
 
 
 class EngineParser:
@@ -8,8 +9,29 @@ class EngineParser:
         self.file = file
 
     def parse(self):
-        pattern_id = re.compile('([A-Z0-9]{11})')
-        pattern_from = re.compile('[a-zA-Z0-9._%+-]+@([a-zA-Z0-9.-]+)(\.[a-zA-Z]{2,4})') #add from in search
-        with open("log") as file_log:
+        pattern_id = re.compile(r'([A-Z0-9]{11})(?=:)')
+        pattern_from = re.compile(r'(?<=from=<)[a-zA-Z0-9._%+-]+@([a-zA-Z0-9.-]+)(\.[a-zA-Z]{2,4})(?=>)')
+        tmp_store = dict()
+        count_ok, count_fail = Counter()
+        st_sent = 'sent'
+        with open(self.file, 'r') as file_log:
             for line in file_log:
                 id_field = pattern_id.match(line)
+                from_field = pattern_from.match(line)
+                if id_field and from_field:
+                    #add key:value to temp dict
+                    tmp_store[id_field] = from_field
+                elif 'status' in line:
+                    #check status in line
+                    if st_sent in line:
+                        count_ok[tmp_store[id_field]] += 1
+                    else:
+                        count_fail[tmp_store[id_field]] += 1
+                elif 'removed' in line:
+                    del tmp_store[id_field]
+                id_field = None
+                from_field = None
+            count_total = count_ok + count_fail
+        return count_ok, count_fail, count_total
+
+
--- parser/parser.py
+++ parser/parser.py
@@ -1,2 +1,9 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
+from .engine import EngineParser
+
+
+
+
+
+
